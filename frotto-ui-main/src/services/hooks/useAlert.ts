c58a4ceab3c0fc5d142873af70cfde994b180d87
Matheus Araújo <matheuspablomello@gmail.com>
2025-12-29 22:06:59 -0300

    correção maintenance

--- frotto-ui-main/src/services/hooks/useAlert.ts
+++ frotto-ui-main/src/services/hooks/useAlert.ts
@@ -1,22 +1,26 @@
 import { useIonToast } from "@ionic/react";
+import { useCallback } from "react";
 
 export const useAlert = () => {
   const [present] = useIonToast();
 
-  const showErrorAlert = (message: string) => {
-    present({
-      message: message,
-      duration: 4000,
-      position: "top",
-      color: "danger",
-      buttons: [
-        {
-          text: "X",
-          role: "cancel",
-        },
-      ],
-    });
-  };
+  const showErrorAlert = useCallback(
+    (message: string) => {
+      present({
+        message: message,
+        duration: 4000,
+        position: "top",
+        color: "danger",
+        buttons: [
+          {
+            text: "X",
+            role: "cancel",
+          },
+        ],
+      });
+    },
+    [present]
+  );
 
   return { showErrorAlert };
 };
